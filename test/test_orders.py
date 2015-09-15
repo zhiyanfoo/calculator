96219298cf6f0983afa3785572b6809122b42037
zhiyanfoo <zhiyanfoo@gmail.com>
2015-09-16 03:21:07 +0800

    Finish parsing numarg for orders

--- test/test_orders.py
+++ test/test_orders.py
@@ -25,26 +25,29 @@ def test_all_valid_values_no_arg(identity):
     assert orders.OrderToken([word]).value == word
 
 
-def test_invalid_token_value():
+def test_invalid_value():
     with pytest.raises(ValueError) as err:
         tok = orders.OrderToken(["prin"])
     assert err.value.args[0] == "Invalid order: prin"
 
-def test_invalid_token_argument():
+def test_invalid_argument():
     with pytest.raises(ValueError) as err:
         tok = orders.OrderToken(["print", 2, 3])
     assert err.value.args[0] == "Order \"print\" given invalid number of arguments (2), args: \"[2, 3]\""
 
     
-def test_valid_token_arguement1():
+def test_valid_arguement_list():
     tok = orders.OrderToken(["remove", "1,11,3"])
     assert tok.value == "remove" \
-            and tok.arg_type == ("list") \
-            and tok.args[0] == (1,11,3)
+            and tok.args_type == ["list"] \
+            and tok.args == [(1,11,3)]
 
-# def test_valid_token_arguement():
-#     tok = orders.OrderToken(["print", "1-5"])
-#     assert tok.value == "print"
-#     assert tok.arg_type == ("range")
-#     assert tok.args == ((1,5))
+def test_valid_arguement_range():
+    tok = orders.OrderToken(["print", "1..5"])
+    assert tok.value == "print" \
+            and tok.args_type == ["range"] \
+            and tok.args == [(1,5)]
+
+# def test_invalid_arguement_range():
+#     tok = orders.OrderToken(["
 
--- zero_cas/orders.py
+++ zero_cas/orders.py
@@ -14,7 +14,7 @@ class OrderToken:
 
     def __init__(self, lexemes):
         self.value = self.valid_value(lexemes[0]) 
-        self.args = self.valid_args(lexemes[1:])
+        self.args, self.args_type = self.valid_args(lexemes[1:])
 
     def valid_value(self, value):
         if value in self.keywords:
@@ -22,21 +22,33 @@ class OrderToken:
         else:
             raise ValueError("Invalid order: {0}".format(value))
 
-    def valid_args(self, args):
-        if len(args) not in self.keywords[self.value]:
-            errmsg = "Order \"{0}\" given invalid number of arguments ({1}), args: \"{2}\"".format(self.value, len(args), args)
+    def valid_args(self, lexeme_args):
+        if len(lexeme_args) not in self.keywords[self.value]:
+            errmsg = "Order \"{0}\" given invalid number of arguments ({1}), args: \"{2}\"".format(self.value, len(lexeme_args), lexeme_args)
             raise ValueError(errmsg)
-        # p_list = re.compile(r"(?:\d+,)*\d")
-        # # p_range = re.compile(r"\d*..\d*")
+        p_list = re.compile(r"^(?:\d+,)*\d$")
+        p_range = re.compile(r"^\d*..\d*$")
+        args = list()
+        args_type = list()
 
-        # for num_arg in args:
-        #     if p_list.match(num_arg) == None:
-        #         errmsg = "\"{0}\" given invalid argument \"{1}\"".format(self.value, num_arg)
-        #         raise ValueError(errmsg)
-        # # return args
+        for num_arg in lexeme_args:
+            if p_list.match(num_arg) is not None:
+                print(p_list.match(num_arg))
+                print(num_arg.split(','))
+                args_type.append("list")
+                print(num_arg)
+                parsed_arg = tuple([int(num) for num in num_arg.split(',')])
+                args.append(parsed_arg)
+            elif p_range.match(num_arg) is not None:
+                print(p_range.match(num_arg))
+                args_type.append("range")
+                print(num_arg)
+                parsed_arg = tuple([int(num) for num in num_arg.split('..')])
+                # if len(parsed_arg) > 2:
+                    # errmsg = "\"{0}\" given invalid number({1}) of arguments \"{2}\", max 2".format(self.value, parsed_arg, num_arg)
 
-
-
-
-
-
+                args.append(parsed_arg)
+            else:
+                errmsg = "\"{0}\" given invalid argument \"{1}\"".format(self.value, num_arg)
+                raise ValueError(errmsg)
+        return args, args_type
